256210e0a55ce4fcd4f40f252fc9717acd9b4ba7
SergeyOstrouhov <89896362764@mail.ru>
2024-01-08 08:11:49 +0300

    Загружаются гиды

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ let api_key = "79ccbf2d-deac-458b-b7af-af4d234c846e";
 let url_orders = new URL('http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/orders');
 
 let routes = [];
+let cur_routes = [];
 let guids = {};
 let route_id;
+let sights = [];
 
 
 function showGuids(guid) {
-    let table = document.getElementById('table_routes');
+    let table = document.getElementById('table_guids');
     body = table.getElementsByTagName('tbody')[0];
     let newRow = table.insertRow(table.rows.length);
     
@@ -17,31 +19,38 @@ function showGuids(guid) {
     let cell2 = newRow.insertCell(1);
     let cell3 = newRow.insertCell(2);
     let cell4 = newRow.insertCell(3);
+    let cell5 = newRow.insertCell(4);
 
     // Задание содержимого ячеек
-    cell1.innerHTML = route['name'];
-    cell2.innerHTML = route['description'];
-    cell3.innerHTML = route['mainObject'];
+    cell1.innerHTML = guid['name'];
+    cell2.innerHTML = guid['language'];
+    cell3.innerHTML = guid['workExperience'];
+    cell4.innerHTML = guid['pricePerHour'];
     let selectButton = document.createElement("button");
     selectButton.textContent = "Выбрать";
-    // selectButton.addEventListener("click", function () {
-    //     selectGuid(this);
-    // });
-    cell4.appendChild(selectButton);
+    selectButton.addEventListener("click", function () {
+        selectGuid(this);
+    });
+    cell5.appendChild(selectButton);
 };
 
 async function addGuids(id) {
     let cur_url = url_routes;
-    path = url_routes.pathname + "/" + id + '/guids';
+    path = url_routes.pathname + "/" + id + '/guides';
     cur_url.pathname = path;
     cur_url.searchParams.append('api_key', api_key);
     const response = await fetch(cur_url);
     let json = await response.json();
     json.forEach(guid => {
-        
+        showGuids(guid);
         console.log(guid);
     });
     
+};
+
+function searchByName() {
+    let query = document.getElementById("searchInput").value;
+
 };
 
 function selectRow(button) {
@@ -59,13 +68,13 @@ function selectRow(button) {
     });
     addGuids(route_id);
     
-}
+};
 
 function addRoute(route) {
     let table = document.getElementById('table_routes');
     body = table.getElementsByTagName('tbody')[0];
     let newRow = table.insertRow(table.rows.length);
-    
+
     // Добавление ячеек
     let cell1 = newRow.insertCell(0);
     let cell2 = newRow.insertCell(1);
@@ -84,29 +93,42 @@ function addRoute(route) {
     cell4.appendChild(selectButton);
 };
 
-async function getListOfRoutes(page = 1) {
+function showRoutes(page = 1) {
+    let name = document.getElementById('searchInput').value;
+    let obj = document.getElementById('select_obj').value;
+    console.log(name, obj);
+    let rec = 3 * (page - 1);
+    if (!name && !obj) {
+        console.log('fsf');
+        for (let i = rec; i < rec + 3; i++) {
+            addRoute(routes[i]);
+        }
+    } else {
+        console.log('sdfsd');
+        let cur_routes = [];
+        routes.forEach(route => {
+            if (route['name'].includes(name) || route['mainObject'].includes(obj)) {
+                cur_routes.push(route);
+            }
+        });
+        for (let i = rec; i < rec + 3 && i < cur_routes.length; i++) {
+            addRoute(cur_routes[i]);
+        }
+    }
+};
+
+async function getListOfRoutes() {
     let cur_url = url_routes;
     cur_url.searchParams.append('api_key', api_key);
     const response = await fetch(cur_url);
     let json = await response.json();
-    // json.forEach(route => {
-    //     routes.append(route);
-    //     console.log(route);
-    // });
     routes = json;
-    // console.log(routes[0]);
-    let rec = 3 * (page - 1);
-    for (let i = rec; i < rec + 3; i++) {
-        addRoute(routes[i]);
-    }
-    
+    showRoutes();
 };
 
-
 window.onload = function () {
-    //downloadData();
-    //document.querySelector('.search-btn').onclick = searchByName;
-    //document.querySelector('.search-btn').onclick = searchByName;
     getListOfRoutes();
-   
+    document.querySelector('.search-btn').onclick = function () {
+        showRoutes();
+    };
 };
